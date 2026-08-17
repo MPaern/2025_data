fdf7f0fc91ef1835eaa422367ae529100311a94e
MPaern <maris.parn@gmail.com>
2026-08-17 23:20:55 +0200

    plots for bats

---
+++ 2025 autumn.R
@@ -0,0 +1,6 @@
+# 2025 autumn data exploration for mist netting
+# find out what pnat and enil did last year
+# pnat what did happen around Rennesøy, NMBU and Stavanger, coastal area etc. (potential mist netting sites)
+# where was Enil later, what are the most promising locations for that species
+# Look what happened to pnat in the suth and in Grødem area- potential for mist netting?
+
